a4696892d3914c87b52930120d826b45f0dfe623
Andrew <andrewlang.p@gmail.com>
2024-10-24 00:06:29 -0400

    add overwrite functionality if file exists

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const needle = require('needle');
+const readline = require('readline');
 
 args = process.argv.slice(2);
 
@@ -9,16 +10,41 @@ const config = {
   path: args[1]
 }
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const fetcher = function(url, path) {
   needle.get(url,(error, response, body) => {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    fs.writeFile(path, body, err => {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log("File written successfully\n")
+    fs.access(path, fs.constants.F_OK, err => {
+      if (!err) {
+        console.log('File already exists.')
+        rl.question('Do you want to overwrite the file? Type y for yes or n for no:  ', (reply) => {
+          if (reply === 'y') {
+            fs.writeFile(path, body, err => {
+              if (err) {
+                console.log(err);
+              }
+              else {
+                console.log("File written successfully\n");
+              }
+            })
+          }
+          console.log("End of question");
+          rl.close();
+        })
+      } else {
+        fs.writeFile(path, body, err => {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log("File written successfully\n");
+          }
+        })
       }
     })
   });
